98cff7369a274fd0dd77979b4c0cdfe68be1539e
SuryaTeja-N <sxn2261@mavs.uta.edu>
2025-02-25 23:18:07 -0600

    configued springboot api

---
+++ api-gateway/src/main/java/com/apisecurity/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.apisecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "API Security Gateway is Running!";
+    }
+}
